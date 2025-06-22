817c613bd497382678cf4f85798e1b75b39c9804
WooshC <140362091+WooshC@users.noreply.github.com>
2025-06-22 13:30:45 -0500

    migraciones

---
+++ Migrations/Clientes/20250622180205_InitialClientes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetCareServicios.Migrations.Clientes
+{
+    /// <inheritdoc />
+    public partial class InitialClientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioID = table.Column<int>(type: "int", nullable: false),
+                    DocumentoIdentidad = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    DocumentoIdentidadArchivo = table.Column<byte[]>(type: "VARBINARY(MAX)", nullable: true),
+                    DocumentoVerificado = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    FechaVerificacion = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_DocumentoIdentidad",
+                table: "Clientes",
+                column: "DocumentoIdentidad");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Cliente_Usuario",
+                table: "Clientes",
+                column: "UsuarioID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
